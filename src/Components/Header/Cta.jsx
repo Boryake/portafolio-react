8f341ce71146f984105652034210f23ff0ad183c
Boryake <facundopasqua@gmail.com>
2022-08-08 12:51:34 -0300

    Provisory solution Cv

--- src/Components/Header/Cta.jsx
+++ src/Components/Header/Cta.jsx
@@ -4,7 +4,7 @@ import CV from "./cv.pdf"
 const Cta = () => {
   return (
     <div className='cta'>
-        <a href={CV} download="Pasqua_Facundo_Cv.pdf" className='btn'>Download CV</a>
+        <a href="https://drive.google.com/file/d/1jqqTrMGFBSv4tf19awNja2wjXsQvd0Sq/view?usp=sharing" download="Pasqua_Facundo_Cv.pdf"  target="_blank" className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )

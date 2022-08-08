0fdd419e700847f592b9468c1a31fcad8a26c009
Boryake <facundopasqua@gmail.com>
2022-08-08 12:33:43 -0300

    m

--- src/Components/Header/Cta.jsx
+++ src/Components/Header/Cta.jsx
@@ -4,7 +4,7 @@ import CV from "./cv.pdf"
 const Cta = () => {
   return (
     <div className='cta'>
-        <a href={CV} download="Pasqua_Facundo_Cv" className='btn'>Download CV</a>
+        <a href={CV} download="Pasqua_Facundo_Cv.pdf" className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )

5cdf2f515a7992fd7a45a52dad2a6374238b2cec
Boryake <facundopasqua@gmail.com>
2022-08-08 11:30:06 -0300

    cv changes

--- src/Components/Header/Cta.jsx
+++ src/Components/Header/Cta.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from "../../assets/assets/cv.pdf"
+import CV from "./cv.pdf"
 
 const Cta = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn' >Download CV </a>
+        <a href={CV} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
